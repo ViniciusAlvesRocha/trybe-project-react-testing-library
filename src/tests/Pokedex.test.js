4b8a464ac6ea6ed729b7c5266aff9f52b27ed94d
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-06-08 13:47:30 -0300

    Consertando Requisito Pokedex

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -3,6 +3,95 @@ import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
 
+const pokemons = [
+  {
+    name: 'Charmander',
+  },
+  {
+    name: 'Caterpie',
+  },
+  {
+    name: 'Ekans',
+  },
+  {
+    name: 'Alakazam',
+  },
+  {
+    name: 'Mew',
+  },
+  {
+    name: 'Rapidash',
+  },
+  {
+    name: 'Snorlax',
+  },
+  {
+    name: 'Dragonair',
+  },
+  {
+    name: 'Pikachu',
+  },
+];
+
+const buttons = [
+  'All',
+  'Electric',
+  'Fire',
+  'Bug',
+  'Poison',
+  'Psychic',
+  'Normal',
+  'Dragon',
+];
+
+const pokemonsByFilter = [
+  {
+    type: 'All',
+    pokemons: [
+      'Charmander',
+      'Caterpie',
+      'Ekans',
+      'Alakazam',
+      'Mew',
+      'Rapidash',
+      'Snorlax',
+      'Dragonair',
+    ],
+  },
+  {
+    type: 'Electric',
+    pokemons: [
+      'Pikachu',
+    ],
+  },
+  {
+    type: 'Fire',
+    pokemons: [
+      'Charmander',
+      'Rapidash',
+    ],
+  },
+  {
+    type: 'Bug',
+    pokemons: [
+      'Caterpie',
+    ],
+  },
+  {
+    type: 'Poison',
+    pokemons: [
+      'Ekans',
+    ],
+  },
+  {
+    type: 'Psychic',
+    pokemons: [
+      'Alakazam',
+      'Mew',
+    ],
+  },
+];
+
 describe('Testing <Pokedex.js> component', () => {
   it('Teste se página contém um heading h2 com o texto Encountered pokémons.', () => {
     const { getByRole } = renderWithRouter(<App />);
@@ -14,35 +103,6 @@ describe('Testing <Pokedex.js> component', () => {
   });
   it(`Testa se é exibido o próximo Pokémon da lista quando
    o botão Próximo pokémon é clicado.`, () => {
-    const pokemons = [
-      {
-        name: 'Charmander',
-      },
-      {
-        name: 'Caterpie',
-      },
-      {
-        name: 'Ekans',
-      },
-      {
-        name: 'Alakazam',
-      },
-      {
-        name: 'Mew',
-      },
-      {
-        name: 'Rapidash',
-      },
-      {
-        name: 'Snorlax',
-      },
-      {
-        name: 'Dragonair',
-      },
-      {
-        name: 'Pikachu',
-      },
-    ];
     const { getByRole, getByText } = renderWithRouter(<App />);
     for (let index = 0; index < pokemons.length; index += 1) {
       const btnNextPokemon = getByRole('button', {
@@ -54,16 +114,6 @@ describe('Testing <Pokedex.js> component', () => {
   });
   it('Teste se a Pokédex tem os botões de filtro', () => {
     const { getByRole } = renderWithRouter(<App />);
-    const buttons = [
-      'All',
-      'Electric',
-      'Fire',
-      'Bug',
-      'Poison',
-      'Psychic',
-      'Normal',
-      'Dragon',
-    ];
     for (let index = 0; index < buttons.length; index += 1) {
       const button = getByRole('button', {
         name: buttons[index],
@@ -73,53 +123,6 @@ describe('Testing <Pokedex.js> component', () => {
   });
   it(`A partir da seleção de um botão de tipo, a Pokédex deve circular
    somente pelos pokémons daquele tipo`, () => {
-    const pokemonsByFilter = [
-      {
-        type: 'All',
-        pokemons: [
-          'Charmander',
-          'Caterpie',
-          'Ekans',
-          'Alakazam',
-          'Mew',
-          'Rapidash',
-          'Snorlax',
-          'Dragonair',
-        ],
-      },
-      {
-        type: 'Eletric',
-        pokemons: [
-          'Pikachu',
-        ],
-      },
-      {
-        type: 'Fire',
-        pokemons: [
-          'Charmander',
-          'Rapidash',
-        ],
-      },
-      {
-        type: 'Bug',
-        pokemons: [
-          'Caterpie',
-        ],
-      },
-      {
-        type: 'Poison',
-        pokemons: [
-          'Ekans',
-        ],
-      },
-      {
-        type: 'Psychic',
-        pokemons: [
-          'Alakazam',
-          'Mew',
-        ],
-      },
-    ];
     const { getByRole, getByText } = renderWithRouter(<App />);
     for (let index = 0; index < pokemonsByFilter; index += 1) {
       const btnFilter = getByRole('button', {
@@ -141,5 +144,49 @@ describe('Testing <Pokedex.js> component', () => {
       name: 'All',
     });
     userEvent.click(btnResetFilter);
+  });
+  it(`Teste se é criado, dinamicamente, 
+  um botão de filtro para cada tipo de Pokémon`, () => {
+    const pokemonTypes = [
+      'Fire',
+      'Psychic',
+      'Electric',
+      'Bug',
+      'Poison',
+      'Dragon',
+      'Normal',
+    ];
+
+    const { getByRole } = renderWithRouter(<App />);
+
+    pokemonTypes.forEach((pokemonType) => {
+      expect(getByRole('button', {
+        name: pokemonType,
+      })).toBeInTheDocument();
+      expect(getByRole('button', {
+        name: 'All',
+      })).toBeInTheDocument();
+    });
+
+    pokemonsByFilter.forEach((item) => {
+      const btnTypePokemon = getByRole('button', {
+        name: item.type,
+      });
+      userEvent.click(btnTypePokemon);
+      const btnProximoPokemon = getByRole('button', {
+        name: /Próximo pokémon/,
+      });
+      if (item.pokemons.length === 1) {
+        expect(btnProximoPokemon.disabled).toBeTruthy();
+      } else {
+        expect(btnProximoPokemon.disabled).not.toBeTruthy();
+      }
+    });
+  });
+  it('Pokemon tipo botão', () => {
+    const numberOfButtons = 7;
+    const { getAllByTestId } = renderWithRouter(<App />);
+    const btn = getAllByTestId('pokemon-type-button');
+    expect(btn.length).toBe(numberOfButtons);
   });
 });
--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,48 @@
-test('', () => {});
+import React from 'react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import { render } from '@testing-library/react';
+
+describe('7. Teste o componente <PokemonDetails.js />', () => {
+  it(`Teste se as informações detalhadas do Pokémon
+  selecionado são mostradas na tela.`, () => {
+    const { getByRole, getAllByRole, queryByText, getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const headingH2 = getByRole('heading', {
+      name: 'Pikachu Details',
+      level: 2,
+    });
+    expect(headingH2).toBeInTheDocument();
+    const linkMoreDetails = queryByText('More details');
+    expect(linkMoreDetails).not.toBeInTheDocument();
+
+    // A seção de detalhes deve conter um heading h2 com o texto Summary
+    const summaryH2 = getByRole('heading', {
+      name: 'Summary',
+      level: 2,
+    });
+    expect(summaryH2).toBeInTheDocument();
+
+    const summaryText = getByText(/This intelligent Pokémon roasts hard/);
+    expect(summaryText).toBeInTheDocument();
+  });
+  it(`Teste se existe na página uma seção com os
+  mapas contendo as localizações do pokémon`, () => {
+    const { getByRole, getByText, history } = renderWithRouter(<App />);
+    history.push('/pokemons/25');
+    const headingH2 = getByRole('heading', {
+      name: 'Game Locations of Pikachu',
+      level: 2,
+    });
+    expect(headingH2).toBeInTheDocument();
+    // Verificando se todas as localizações do pokemon estão presentes na seção de detalhes
+    const firstLocation = getByText('Kanto Viridian Forest');
+    expect(firstLocation).toBeInTheDocument();
+    const secondLocation = getByText('Kanto Power Plant');
+    expect(secondLocation).toBeInTheDocument();
+
+    const imgsLocation = getAllByRole('img');
+    console.log();
+  });
+});
