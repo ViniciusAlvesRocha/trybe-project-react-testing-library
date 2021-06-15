2f04ddc9ea2248112ee6c4099024fac946485ace
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-06-14 22:54:50 -0300

    Consertando requisito 7 - PokemonsDetails

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -2,18 +2,20 @@ import React from 'react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../renderWithRouter';
 import App from '../App';
-import { render } from '@testing-library/react';
 
 describe('7. Teste o componente <PokemonDetails.js />', () => {
+  const POKEMONS_25 = '/pokemons/25';
   it(`Teste se as informações detalhadas do Pokémon
   selecionado são mostradas na tela.`, () => {
-    const { getByRole, getAllByRole, queryByText, getByText, history } = renderWithRouter(<App />);
-    history.push('/pokemons/25');
+    const { getByRole, queryByText, getByText, history } = renderWithRouter(<App />);
+    history.push(POKEMONS_25);
     const headingH2 = getByRole('heading', {
       name: 'Pikachu Details',
       level: 2,
     });
     expect(headingH2).toBeInTheDocument();
+
+    /* Não deve existir o link de navegação para os detalhes do Pokémon selecionado. */
     const linkMoreDetails = queryByText('More details');
     expect(linkMoreDetails).not.toBeInTheDocument();
 
@@ -29,8 +31,13 @@ describe('7. Teste o componente <PokemonDetails.js />', () => {
   });
   it(`Teste se existe na página uma seção com os
   mapas contendo as localizações do pokémon`, () => {
-    const { getByRole, getByText, history } = renderWithRouter(<App />);
-    history.push('/pokemons/25');
+    const {
+      getByRole,
+      getAllByRole,
+      getByText,
+      history,
+      getAllByAltText } = renderWithRouter(<App />);
+    history.push(POKEMONS_25);
     const headingH2 = getByRole('heading', {
       name: 'Game Locations of Pikachu',
       level: 2,
@@ -42,7 +49,42 @@ describe('7. Teste o componente <PokemonDetails.js />', () => {
     const secondLocation = getByText('Kanto Power Plant');
     expect(secondLocation).toBeInTheDocument();
 
-    const imgsLocation = getAllByRole('img');
-    console.log();
+    /* Devem ser exibidos, o nome da localização e
+    uma imagem do mapa em cada localização; */
+
+    const imagesLocations = getAllByRole('img');
+
+    expect(imagesLocations[1].src).toBe('https://cdn2.bulbagarden.net/upload/0/08/Kanto_Route_2_Map.png');
+
+    expect(imagesLocations[2].src).toBe('https://cdn2.bulbagarden.net/upload/b/bd/Kanto_Celadon_City_Map.png');
+
+    /* A imagem da localização deve ter um atributo alt
+    com o texto <name> location, onde <name> é o nome do Pokémon; */
+    const altTextPikachuLocation = getAllByAltText(/Pikachu location/);
+    expect(altTextPikachuLocation.length).toBe(2);
+  });
+  it(`Teste se o usuário pode favoritar 
+  um pokémon através da página de detalhes.`, () => {
+    const { getByLabelText, getByAltText, history } = renderWithRouter(<App />);
+    history.push(POKEMONS_25);
+    /* A página deve exibir um checkbox que
+    permite favoritar o Pokémon; */
+    const inputCheckbox = getByLabelText(/Pokémon favoritado/);
+    expect(inputCheckbox).toBeInTheDocument();
+    /* Cliques alternados no checkbox devem adicionar
+    e remover respectivamente o Pokémon da lista de
+    favoritos; */
+    userEvent.click(inputCheckbox);
+    history.push(/favorites/);
+
+    /* Verificando se o pikachu é adicionado
+    a lista de favoritos: */
+    const pikachuInFavoritePokemons = getByAltText(/Pikachu sprite/);
+    expect(pikachuInFavoritePokemons).toBeInTheDocument();
+
+    /* history.push('/pokemons/25');
+    userEvent.click(inputCheckbox);
+    history.push(/favorites/);
+    expect(pikachuInFavoritePokemons).not.toBeInTheDocument(); */
   });
 });
